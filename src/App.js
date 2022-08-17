0e01865d20fbf409856cd7ab36afcd5fd2cb8e56
Ashley <ashleyxcodes@gmail.com>
2022-08-17 10:52:04 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+
       <footer>
         This project was coded by Ashley Brianna and is {""}
         <a
