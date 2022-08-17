f42247811768760c250e0d5e9443a2e676d7ecfb
Ashley <ashleyxcodes@gmail.com>
2022-08-17 08:50:21 -0700

    added GitHub repo URL.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Ashley Brianna and is {""}
+        <a
+          href="https://github.com/Ashleyxbrianna/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-Sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
