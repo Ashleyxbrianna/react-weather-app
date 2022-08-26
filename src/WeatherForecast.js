eecdffeb594c70f819f2dd3fafe52b4daf6a59c1
Ashley <ashleyxcodes@gmail.com>
2022-08-26 16:56:24 -0700

    added a new function - load

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,6 +15,16 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "04346521acf1ede91479e6065f990fb9";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric
+`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -33,13 +43,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "04346521acf1ede91479e6065f990fb9";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric
-`;
-
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
